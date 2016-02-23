bd7a9c2f411a91217eba16e85eb7c9e424651a41
Michael Xavier <michael@michaelxavier.net>
2016-02-22 21:26:48 -0800

    Handle ziplists

--- generate_dump.sh
+++ generate_dump.sh
@@ -5,8 +5,37 @@ set -e
 redis-server test/data/example.conf
 cmd="redis-cli -p 6378"
 
+# strings in the default database
 $cmd -n 0 SET foo fooval
+# integers
+$cmd -n 0 INCRBY num 42
+# TODO: i think redis supports floating point now?
+# lists
+$cmd -n 0 RPUSH list one two
+# empty lists
+# i'm not actually sure that empty lists/sets/hashes will get persisted
+$cmd -n 0 RPUSH emptylist shortlived
+$cmd -n 0 RPOP emptylist
+# sets
+$cmd -n 0 SADD set one two
+# empty sets
+$cmd -n 0 SADD emptyset shortlived
+$cmd -n 0 SPOP emptyset
+# hashes
+# $cmd -n 0 HSET hash str stringval
+# $cmd -n 0 HINCRBY hash int 42
+# empty hashes
+# $cmd -n 0 HSET emptyhash k shortlived
+# $cmd -n 0 HDEL emptyhash k
+# sorted sets
+# $cmd -n 0 ZADD zset 2 lastplace 1 firstplace
+# empty sorted sets
+# $cmd -n 0 ZADD emptyzset 1 shortlived
+# $cmd -n 0 ZREM emptyzset shortlived
+# strings in another database
 $cmd -n 1 SET bar barval
+#TODO: timestamps? have to se them wayyy in the future
+
 $cmd SAVE
 
 kill $(cat test/data/redis.pid)
--- src/Database/Redis/RDB.hs
+++ src/Database/Redis/RDB.hs
@@ -11,6 +11,8 @@ module Database.Redis.RDB
     , RedisString(..)
     , RedisList(..)
     , RedisSet(..)
+    , RedisZiplist(..)
+    , RedisZLEntry(..)
     , EOF(..)
     , RCRC64(..)
     -- * Parsers
@@ -34,16 +36,17 @@ import           Control.Monad
 import           Control.Monad.Catch
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Resource
+import           Data.Attoparsec.Binary
 import           Data.Attoparsec.ByteString   as A
 import           Data.Bits
 import           Data.ByteString              (ByteString)
-import qualified Data.ByteString              as BS
+import           Data.Int
+-- import qualified Data.ByteString              as BS
 import qualified Data.ByteString.Char8        as BS8
 import           Data.Conduit
 import           Data.Conduit.Attoparsec
 import           Data.Conduit.Binary
 import qualified Data.Conduit.List            as CL
-import           Data.List
 import           Data.Monoid
 import           Data.Ratio
 import           Data.Set                     (Set)
@@ -159,6 +162,8 @@ data RedisString = RString !ByteString
 data RedisValue = RVString !RedisString
                 | RVList !RedisList
                 | RVSet !RedisSet
+                | RVIntSet !RedisIntSet
+                | RVZiplist !RedisZiplist
                 deriving (Show, Eq)
 
 
@@ -170,6 +175,30 @@ newtype RedisList = RedisList {
 newtype RedisSet = RedisSet {
       redisSet :: Set ByteString
     } deriving (Show, Eq)
+
+
+data RedisZiplist = RedisZiplist {
+      zlBytes   :: !Word32
+    , zlTail    :: !Word32
+    , zlLen     :: !Word16
+    , zlEntries :: ![RedisZLEntry]
+    } deriving (Show, Eq)
+
+
+--TODO: we should determine if the goal here is to coerce to
+--reasonable haskell types or to keep it very close to internal
+--representation for the purpose of counting bits.
+data RedisZLEntry = RedisZLEntry {
+      zlePrevEntryLength :: !(Either Word8 Word32)
+    , zleValue           :: !RedisString
+    } deriving (Show, Eq)
+
+
+--TODO: is the intset unsigned or signed?
+data RedisIntSet = RedisIntSet16 !(Set Word16)
+                 | RedisIntSet32 !(Set Word32)
+                 | RedisIntSet64 !(Set Word64)
+                 deriving (Show, Eq)
 
 
 data RCRC64 = RCRC64 {
@@ -203,28 +232,30 @@ rdbDatabaseSelectorP = do
 
 
 -------------------------------------------------------------------------------
-integerAsStringP :: Parser Int
-integerAsStringP = do
+-- | Selects only the int-oriented variants of length encoding
+intLengthEncodingP :: Parser Int
+intLengthEncodingP = do
   enc <- lengthEncoding
   case enc of
+    Length n           -> return n
     IntegerAsString sz -> integerAsString sz
-    _ -> fail "Expected integer as string"
+    _                  -> fail "Expected integer as string"
 
 
 -------------------------------------------------------------------------------
 integerAsString :: IntSize -> Parser Int
 integerAsString Int8Size = fromIntegral <$> anyWord8
-integerAsString Int16Size = fromIntegral <$> takeWord16
-integerAsString Int32Size = fromIntegral <$> takeWord32
+integerAsString Int16Size = fromIntegral <$> anyWord16be
+integerAsString Int32Size = fromIntegral <$> anyWord32be
 
 
 -------------------------------------------------------------------------------
 kvPairP :: Parser KVPair
 kvPairP = do
-  --ts <- trace "trying kvPairP" $ optional timestampP
+  ts <- trace "trying kvPairP" $ optional timestampP
   --_ <- anyWord8 -- maybe timestampP has to consume the byte anyways
-  let ts = Nothing
-  vt <- trace "skip ts" redisValueTypeP
+  -- let ts = Nothing
+  vt <- redisValueTypeP
   --TODO: i think rsk may just be straight length encoded
   rsk <- traceShow ("value type" :: String, vt) redisStringP
   k <- case traceShow ("rsk" :: String, rsk) rsk of
@@ -236,23 +267,119 @@ kvPairP = do
 
 -------------------------------------------------------------------------------
 parseVal :: RedisValueType -> Parser RedisValue
-parseVal StringEnc = RVString <$> redisStringP
-parseVal ListEnc   = RVList <$> redisListP
-parseVal SetEnc    = RVSet <$> redisSetP
-parseVal _         = undefined
+parseVal StringEnc           = RVString <$> redisStringP
+parseVal ListEnc             = RVList <$> redisListP
+parseVal SetEnc              = RVSet <$> redisSetP
+parseVal ZSetEnc             = error "TODO: ZSetEnc"
+parseVal HashEnc             = error "TODO: HashEnc"
+parseVal ZipmapEnc           = error "TODO: ZipmapEnc"
+parseVal ZiplistEnc          = RVZiplist <$> redisZiplistP
+parseVal IntsetEnc           = error "TODO: IntsetEnc"
+parseVal ZSetInZiplistEnc    = error "TODO: ZSetInZiplistEnc"
+parseVal HashmapInZiplistEnc = error "TODO: HashmapInZiplistEnc"
 
 
 -------------------------------------------------------------------------------
 redisListP :: Parser RedisList
 redisListP = do
-  len <- integerAsStringP
-  traceShow ("len" :: String, len) ( RedisList <$> replicateM len redisStringOnlyP)
+  len <- intLengthEncodingP
+  traceShow ("llen" :: String, len) ( RedisList <$> replicateM len redisStringOnlyP)
 
 
 -------------------------------------------------------------------------------
 redisSetP :: Parser RedisSet
 redisSetP = RedisSet . S.fromList . redisList <$> redisListP
 
+
+-------------------------------------------------------------------------------
+redisZiplistP :: Parser RedisZiplist
+redisZiplistP = either fail return . parseOnly parseEnvelope =<< redisStringOnlyP
+  where
+    parseEnvelope = do
+      zlb <- anyWord32le
+      zlt <- anyWord32le
+      zll <- anyWord16le
+      entries <- replicateM (fromIntegral zll) redisZLEntryP
+      _zlend <- word8 255
+      return (RedisZiplist zlb zlt zll entries)
+
+
+-------------------------------------------------------------------------------
+redisZLEntryP :: Parser RedisZLEntry
+redisZLEntryP = do
+  lenFirst <- anyWord8
+  prevLen <- if lenFirst <= 253
+               then return (Left lenFirst)
+               else Right <$> anyWord32be
+  firstByte <- testBit8 <$> peekWord8'
+  str <- case firstByte of
+    (O,O,_,_,_,_,_,_) -> fmap RString . A.take . fromIntegral =<< anyWord8
+    (O,I,_,_,_,_,_,_) -> do -- take 16 bits, ignore these first two, that's strlen
+      len14 <- (`clearBit` 14) <$> anyWord16be -- only need to set this one I to off
+      RString <$> A.take (fromIntegral len14)
+    (I,O,_,_,_,_,_,_) -> do
+      _ <- anyWord8 -- trash this byte we're focusing on, take next 4
+      fmap RString . A.take . fromIntegral =<< anyWord32be
+    (I,I,O,O,_,_,_,_) -> -- next 2 bytes are 16 bit signed
+      fmap RString . A.take . fromIntegral . w16i =<< anyWord16be
+    (I,I,O,I,_,_,_,_) -> -- next 4 bytes are 32 bit signed
+      fmap RString . A.take . fromIntegral . w32i =<< anyWord32be
+    (I,I,I,O,_,_,_,_) -> -- next 8 bytes are 64 bit signed
+      fmap RString . A.take . fromIntegral . w64i =<< anyWord64be
+    (I,I,I,I,O,O,O,O) -> -- next 3 bytes as 24 bit signed integer
+      fmap RString . A.take . fromIntegral . i24i =<< anyInt24BE
+    (I,I,I,I,I,I,I,O) -> -- next byte as 8 bit signed
+      fmap RString . A.take . fromIntegral . w8i =<< anyWord8
+    (I,I,I,I,_,_,_,_) -> -- remaining is unsigned int 4 from 0000 to 1101 - 1
+      -- take off first 4 bits with 0xf mask
+      fmap RString . A.take . fromIntegral . pred . (0xf .&.) =<< anyWord8
+  return (RedisZLEntry prevLen str)
+
+
+-------------------------------------------------------------------------------
+newtype Int24BE = Int24BE (Word8, Word8, Word8)
+
+
+anyInt24BE :: Parser Int24BE
+anyInt24BE = Int24BE <$> ((,,) <$> anyWord8 <*> anyWord8 <*> anyWord8)
+
+
+--uh how do we know we're gettign this signage right?
+i24i :: Int24BE -> Int32
+i24i (Int24BE (w1, w2, w3)) = foldr unstep 0 [w1, w2, w3]
+  where
+    unstep b a = a `shiftL` 8 .|. fromIntegral b
+
+
+-------------------------------------------------------------------------------
+w8i :: Word8 -> Int8
+w8i = fromIntegral
+
+
+-------------------------------------------------------------------------------
+w16i :: Word16 -> Int16
+w16i = fromIntegral
+
+
+-------------------------------------------------------------------------------
+w32i :: Word32 -> Int32
+w32i = fromIntegral
+
+
+-------------------------------------------------------------------------------
+w64i :: Word64 -> Int64
+w64i = fromIntegral
+
+
+-------------------------------------------------------------------------------
+-- | Cute way of modeling readable masks
+data B = O | I
+
+
+testBit8 :: Word8 -> (B,B,B,B,B,B,B,B)
+testBit8 w = (t 7, t 6, t 5, t 4, t 3, t 2, t 1, t 0)
+  where
+    t n = if testBit w n then I else O
 
 -------------------------------------------------------------------------------
 timestampP :: Parser POSIXTime
@@ -271,7 +398,7 @@ eofP = EOF <$ word8 0xff
 
 -------------------------------------------------------------------------------
 checksumP :: Parser RCRC64
-checksumP = RCRC64 . fromIntegral <$> takeWord64
+checksumP = RCRC64 . fromIntegral <$> anyWord64be
 
 
 -------------------------------------------------------------------------------
@@ -292,15 +419,16 @@ data IntSize = Int8Size
 lengthEncoding :: Parser LengthEncoding
 lengthEncoding = do
   firstByte <- anyWord8
-  let twoMSB = (testBit firstByte 7, testBit firstByte 6)
-  case traceShow ("firstByte" :: String, firstByte, "twoMSB" :: String, twoMSB) twoMSB of
+  --TODO: use 8 bit fields trick for better readability
+  let twoMSB =  (testBit firstByte 7, testBit firstByte 6)
+  case twoMSB of
     (False, False) -> return (Length (fromIntegral (clear2MSB firstByte))) -- next 6 are length
     (False, True) -> do -- read another byte and combine all 14 bits for length
       nextByte :: Word16 <- fromIntegral <$> anyWord8
       let msb6Bits = fromIntegral (clear2MSB firstByte)
       return (Length (fromIntegral ((msb6Bits `shiftL` 8) + nextByte)))
     (True, False) -> do -- discard next 6 bits, read 4 bytes for the length big endian
-      Length . fromIntegral <$> takeWord32
+      Length . fromIntegral <$> anyWord32be
     (True, True) -> case traceShow ("clear2MSB firstByte" :: String, firstByte, "->" :: String, clear2MSB firstByte) clear2MSB firstByte of
                       0 -> return (IntegerAsString Int8Size)
                       1 -> return (IntegerAsString Int16Size)
@@ -311,30 +439,6 @@ lengthEncoding = do
   where
     clear2MSB :: Word8 -> Word8
     clear2MSB n = n `clearBit` 7 `clearBit` 6
-
-
--------------------------------------------------------------------------------
-takeWord16 :: Parser Word16
-takeWord16 = do
-  twoW8s <- fmap fromIntegral . BS.unpack <$> A.take 2
-  let w32s = zipWith (\n shifts -> n `shiftL` (8 * shifts)) twoW8s [1,0]
-  return (foldl' (+) 0 w32s)
-
-
--------------------------------------------------------------------------------
-takeWord32 :: Parser Word32
-takeWord32 = do
-  fourW8s <- fmap fromIntegral . BS.unpack <$> A.take 4
-  let w32s = zipWith (\n shifts -> n `shiftL` (8 * shifts)) fourW8s [3,2,1,0]
-  return (foldl' (+) 0 w32s)
-
-
--------------------------------------------------------------------------------
-takeWord64 :: Parser Word64
-takeWord64 = do
-  fourW8s <- fmap fromIntegral . BS.unpack <$> A.take 4
-  let w64s = zipWith (\n shifts -> n `shiftL` (8 * shifts)) fourW8s [7,6,5,4,3,2,1,0]
-  return (foldl' (+) 0 w64s)
 
 
 -------------------------------------------------------------------------------
@@ -378,16 +482,16 @@ redisValueTypeP :: Parser RedisValueType
 redisValueTypeP = do
   n <- anyWord8
   case traceShow ("n" :: String, n) n of
-    0  -> return StringEnc
-    1  -> return ListEnc
-    2  -> return SetEnc
-    3  -> return ZSetEnc
-    4  -> return HashEnc
-    9  -> return ZipmapEnc
-    10 -> return ZiplistEnc
-    11 -> return IntsetEnc
-    12 -> return ZSetInZiplistEnc
-    13 -> return HashmapInZiplistEnc
+    0x0  -> return StringEnc
+    0x1  -> return ListEnc
+    0x2  -> return SetEnc
+    0x3  -> return ZSetEnc
+    0x4  -> return HashEnc
+    0x9  -> return ZipmapEnc
+    0xa -> return ZiplistEnc
+    0xb -> return IntsetEnc
+    0xc -> return ZSetInZiplistEnc
+    0xd -> return HashmapInZiplistEnc
     _  -> fail ("Unknown redis value type " <> show n)
 
 
